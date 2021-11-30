45d08757f9d2bc66bf156ba4c047de4f4d2ad5fd
Gregory Tucker <gregory.tucker@gmail.com>
2021-11-30 06:02:21 +0100

    Workflow 'needs' numpy to run tests

--- src/version.hpp
+++ src/version.hpp
@@ -3,13 +3,13 @@
 //! \file
 namespace project::version{
     //! `project` git repository revision information at build time
-    auto constexpr git_revision = u8"d809621bfce537b639bc9f60a1d67d94afb6b15c";
+    auto constexpr git_revision = u8"e1db50ca82e8819ab8f5808661d31b832fec7613";
     //! `project` git repository branch at build time
     auto constexpr git_branch = u8"master";
     //! build date and time in YYYY-MM-DDThh:mm format
-    auto constexpr build_datetime = u8"2021-11-29T22:51";
+    auto constexpr build_datetime = u8"2021-11-30T05:56";
     //! `project` version
-    auto constexpr version_number = u8"0.0.1";
+    auto constexpr version_number = u8"0.0.3";
     //! hostname of the build machine
     auto constexpr build_hostname = u8"yolin";
 }
