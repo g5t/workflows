354c8f7a55d0bfb99bc3d41c3369d0f745f6efe1
Gregory Tucker <gregory.tucker@ess.eu>
2023-01-16 15:45:12 +0100

    [Fix] test should import the correct module

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,13 @@
-import cmake_example as m
-
+import module as m
 
 def test_main():
-    assert m.__version__ == "0.0.1"
     assert m.add(1, 2) == 3
     assert m.subtract(1, 2) == -1
+
+def test_module():
+    import module._module as cm
+    assert hasattr(cm, '__version__')
+    assert hasattr(cm, '__build_datetime')
+    assert hasattr(cm, '__build_hostname')
+    assert hasattr(cm, '__git_branch')
+    assert hasattr(cm, '__git_revision')
