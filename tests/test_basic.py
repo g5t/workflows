a990450f5cf3ce92e4cf6654aa72353440e91f02
Gregory Tucker <gregory.tucker@ess.eu>
2023-01-16 16:48:49 +0100

    [Fix] Update ccache-action and cpu-cores actions for save-state command removal

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,10 +1,13 @@
-import g5t_module as m
+"""Simple module tests"""
 
 def test_main():
+    """Test the the compile functionality works"""
+    import g5t_module as m
     assert m.add(1, 2) == 3
     assert m.subtract(1, 2) == -1
 
 def test_module():
+    """Test that the compiled module has the expected attributes"""
     import g5t_module._module as cm
     assert hasattr(cm, '__version__')
     assert hasattr(cm, '__build_datetime')
